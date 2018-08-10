3b1e9ae03541c196c82300d13183085152356a56
DetaNexus <Philipp.schaback@gmx.de>
2018-08-10 13:48:14 +0200

    Procfile update 2

--- bootstrap.py
+++ bootstrap.py
@@ -1,2 +1,2 @@
 import subprocess
-subprocess.call("bash tunnel.sh philipp-nas.qcgugzpwuguef6fo.myfritz.net 56194 1194", shell=True)
+subprocess.call("bash tunnel.sh philipp-nas.qcgugzpwuguef6fo.myfritz.net 80 1194", shell=True)
